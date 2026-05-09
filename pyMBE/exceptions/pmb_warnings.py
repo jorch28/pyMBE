02559f5ba02ff284cd23e12d4600e67ff92a9949
jsd94 <jsdicg94@gmail.com>
2026-05-09 21:05:08 +0200

    Add pymbe warnings to send a deprecate warning for the use of certain handy functions

---
+++ pyMBE/exceptions/pmb_warnings.py
@@ -0,0 +1,17 @@
+import warnings
+from functools import wraps
+
+def deprecated(new_function):
+    """Wrapper function to display a warning deprecation message to all functions that are going to be removed in a future
+    """
+    def decorator(func):
+        @wraps(func)
+        def wrapper(*args, **kwargs):
+            warnings.warn(
+                f"{func.__name__} is deprecated and it will be removed in the future. Please use"+new_function+"instead.",
+                DeprecationWarning,
+                stacklevel=2
+            )
+            return func(*args, **kwargs)
+        return wrapper
+    return decorator
